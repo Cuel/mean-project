28d90d6ba07d72743f8cc2aa4a622412e02bb345
Cuel <alleee@gmail.com>
2015-01-30 01:12:59 +0100

    fix for selecting, fastclick

--- public/app.js
+++ public/app.js
@@ -1,66 +1,74 @@
-angular.module('app', [
+if ('addEventListener' in document) {
+    document.addEventListener('DOMContentLoaded', function() {
+        FastClick.attach(document.body);
+    }, false);
+}
 
-]).controller('appNavigationMenu', ['$scope', '$location', '$window', '$timeout',
-        function ($scope, $location, $window, $timeout) {
+angular.module('app', [])
+    .controller('appNavigationMenu', ['$scope', '$location', '$window', '$timeout',
+    function ($scope, $location, $window, $timeout) {
 
-        $scope.mobileMenuActive = false;
-
-        $scope.links = [{
-            url: 'news'
-                    }, {
-            url: 'home'
-                    }, {
-            url: 'contact'
-                    }, {
-            url: 'about',
-            divider: true
+            $scope.mobileMenuActive = false;
+            $scope.links = [{
+                url: 'news'
+        }, {
+                url: 'home'
+        }, {
+                url: 'contact'
+        }, {
+                divider: true,
+                url: 'about'
         }];
 
-        $scope.toggleMobileMenu = function (linkedClicked) {
-            if (linkedClicked) {
-                if ($scope.mobileMenuActive) {
-                    $scope.mobileMenuActive = false;
+            $scope.toggleMobileMenu = function (linkedClicked) {
+                if (linkedClicked) {
+                    if ($scope.mobileMenuActive) {
+                        $scope.mobileMenuActive = false;
+                    }
+                } else {
+                    $scope.mobileMenuActive = !$scope.mobileMenuActive;
                 }
-            } else {
-                $scope.mobileMenuActive = !$scope.mobileMenuActive;
+            };
+
+            $scope.isActive = function (page) {
+                var currentRoute = $location.path().substring(1) || 'news';
+                return page === currentRoute;
+            };
+
+            // In case the menu is open and the window resized, we need  to manually remove the 
+            // active class from the layout
+            var handleMatchMedia = function (mql) {
+                if (mql.matches) {
+                    $scope.mobileMenuActive = false;
+                    $timeout(function () {
+                        $scope.$apply();
+                    }, 1);
+                }
+            };
+            if ($window.matchMedia) {
+                var mql = $window.matchMedia('(min-width: 48em)');
+                mql.addListener(handleMatchMedia);
+                handleMatchMedia(mql);
             }
-        };
+            // else? who cares about IE
 
-        $scope.isActive = function (page) {
-            var currentRoute = $location.path().substring(1) || 'news';
-            return page === currentRoute;
-        };
+            $scope.$on('$destroy', function () {
+                mql.removeListener(handleMatchMedia);
+            });
 
-        // In case the menu is open and the window resized, we need  to manually remove the 
-        // active class from the layout
-        var handleMatchMedia = function (mql) {
-            if (mql.matches) {
-                $scope.mobileMenuActive = false;
-                $timeout(function () {
-                    $scope.$apply();
-                }, 1);
-            }
-        };
-        if ($window.matchMedia) {
-            var mql = $window.matchMedia('(min-width: 48em)');
-            mql.addListener(handleMatchMedia);
-            handleMatchMedia(mql);
-        }
-        // else? who cares about IE
+}])
 
-        $scope.$on('$destroy', function () {
-            mql.removeListener(handleMatchMedia);
-        });
-    }]).filter('capitalize', function () {
+.filter('capitalize', function () {
     return function (str) {
         return str.substring(0, 1).toUpperCase() + str.substring(1);
     };
-}).directive('blurAfterClick', function ($timeout) {
+})
+
+.directive('blurAfterClick', function ($timeout) {
     return {
         link: function (scope, element) {
             element.bind('click', function () {
-                element.triggerHandler('blur');
-
+                element[0].blur();
             });
         }
     };
